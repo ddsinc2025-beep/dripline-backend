10534ffe3299012af3ebb3f2b745d36b677570ce
decarlos Stewart <ddsinc2025@gmail.com>
2026-05-11 03:05:07 -0400

    add twilio webhook

--- server.js
+++ server.js
@@ -8,11 +8,26 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({
     status: "Dripline backend running"
   });
+});
+
+app.post("/webhook", (req, res) => {
+  const incomingMessage = req.body.Body || "No message received";
+
+  console.log("Incoming SMS:", incomingMessage);
+
+  const reply = `
+<Response>
+  <Message>Dripline received your message: ${incomingMessage}</Message>
+</Response>`;
+
+  res.type("text/xml");
+  res.send(reply);
 });
 
 const PORT = process.env.PORT || 3000;
